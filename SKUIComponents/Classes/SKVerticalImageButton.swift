0df7082e5de6f84b2d6864f24409297fa302023e
Daniel Lee <sihekuang@gmail.com>
2018-02-03 21:08:38 -0800

    Added vertical image button example

---
+++ SKUIComponents/Classes/SKVerticalImageButton.swift
@@ -0,0 +1,65 @@
+//
+//  SKVerticalImageButton.swift
+//  SKUIComponents
+//
+//  Created by Daniel Lee on 2/3/18.
+//
+
+import UIKit
+
+@IBDesignable
+class SKVerticalImageButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    @IBInspectable
+    var imageTextPadding: CGFloat = 6.0
+    
+    @IBInspectable
+    var isImageOnTop: Bool = true
+    
+    override func draw(_ rect: CGRect) {
+        alignImageAndTitleVertically(padding: imageTextPadding)
+    }
+    
+    func alignImageAndTitleVertically(padding: CGFloat) {
+        let imageSize = self.imageView!.frame.size
+        let titleSize = self.titleLabel!.frame.size
+       
+        if isImageOnTop {
+            self.imageEdgeInsets = UIEdgeInsets(
+                top: -titleSize.height,
+                left: 0,
+                bottom: 0,
+                right: -titleSize.width
+            )
+            
+            self.titleEdgeInsets = UIEdgeInsets(
+                top: 0,
+                left: -imageSize.width,
+                bottom: -imageSize.height,
+                right: 0
+            )
+        }else{
+            self.imageEdgeInsets = UIEdgeInsets(
+                top: 0,
+                left: 0,
+                bottom: -titleSize.height,
+                right: -titleSize.width
+            )
+            
+            self.titleEdgeInsets = UIEdgeInsets(
+                top: 0,
+                left: -imageSize.width,
+                bottom: imageSize.height,
+                right: 0
+            )
+        }
+    }
+}
